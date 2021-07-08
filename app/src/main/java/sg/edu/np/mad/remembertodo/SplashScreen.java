5c92ccaca2c3e098ad15ad84e06e485fa35ade34
Lester Cheong <s10208233@connect.np.edu.sg>
2021-07-08 18:44:05 +0800

    themes.xml set to remove all Action Bars, SplashScreen Activity Added and set to launch on startup

---
+++ app/src/main/java/sg/edu/np/mad/remembertodo/SplashScreen.java
@@ -0,0 +1,16 @@
+package sg.edu.np.mad.remembertodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
