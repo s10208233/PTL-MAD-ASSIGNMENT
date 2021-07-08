51a9570c9fe7b5d787000df409abeac5c541cdff
Lester Cheong <s10208233@connect.np.edu.sg>
2021-07-08 19:55:00 +0800

    Fixed timer starting new activity when trying to go from SplashScreen to Home

--- app/src/main/java/sg/edu/np/mad/remembertodo/SplashScreen.java
+++ app/src/main/java/sg/edu/np/mad/remembertodo/SplashScreen.java
@@ -2,9 +2,13 @@ package sg.edu.np.mad.remembertodo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.os.CountDownTimer;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -12,5 +16,11 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Timer().schedule(new TimerTask(){
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, Home.class));
+            }
+        }, 2500 );
     }
 }
