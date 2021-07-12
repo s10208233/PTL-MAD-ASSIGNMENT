023a56d5ae34d7cd67a946cec5c19b27fe11e684
Lester Cheong <s10208233@connect.np.edu.sg>
2021-07-13 02:09:34 +0800

    TaskCategory, Task, Models Created

---
+++ app/src/main/java/sg/edu/np/mad/remembertodo/Task.java
@@ -0,0 +1,50 @@
+package sg.edu.np.mad.remembertodo;
+
+import java.util.Date;
+
+public class Task {
+
+    private String TaskName;
+    private Date DateCreated;
+    private Date DueDate;
+    private boolean Completed;
+
+    public Task(String TaskName, Date DateCreated, Date DueDate, boolean Completed){
+        this.TaskName = TaskName;
+        this.DateCreated = DateCreated;
+        this.DueDate = DueDate;
+        this.Completed = Completed;
+    }
+
+    public String getTaskName() {
+        return TaskName;
+    }
+
+    public void setTaskName(String taskName) {
+        TaskName = taskName;
+    }
+
+    public Date getDateCreated() {
+        return DateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        DateCreated = dateCreated;
+    }
+
+    public Date getDueDate() {
+        return DueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        DueDate = dueDate;
+    }
+
+    public boolean isCompleted() {
+        return Completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        Completed = completed;
+    }
+}
---
+++ app/src/main/java/sg/edu/np/mad/remembertodo/TaskCategory.java
@@ -0,0 +1,40 @@
+package sg.edu.np.mad.remembertodo;
+
+import java.util.ArrayList;
+
+public class TaskCategory {
+
+    private String TaskCategoryName;
+    private ArrayList<Task> TaskList = new ArrayList<>();
+    private String ColorCode;
+
+    public TaskCategory(String TaskCategoryName, ArrayList<Task> TaskList, String ColorCode){
+        this.TaskCategoryName = TaskCategoryName;
+        this.TaskList = TaskList;
+        this.ColorCode = ColorCode;
+    }
+
+    public String getTaskCategoryName() {
+        return TaskCategoryName;
+    }
+
+    public void setTaskCategoryName(String taskCategoryName) {
+        TaskCategoryName = taskCategoryName;
+    }
+
+    public ArrayList<Task> getTaskList() {
+        return TaskList;
+    }
+
+    public void setTaskList(ArrayList<Task> taskList) {
+        TaskList = taskList;
+    }
+
+    public String getColorCode() {
+        return ColorCode;
+    }
+
+    public void setColorCode(String colorCode) {
+        ColorCode = colorCode;
+    }
+}
