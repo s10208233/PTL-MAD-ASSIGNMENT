21994a66449686d774f3ff8396d66dbee4d7f843
Keng Shion <s10204765@connect.np.edu.sg>
2021-07-19 17:40:56 +0800

    tried to add nested remote view but not working :(

--- app/src/main/java/sg/edu/np/mad/remembertodo/DataProvider.java
+++ app/src/main/java/sg/edu/np/mad/remembertodo/DataProvider.java
@@ -2,14 +2,13 @@ package sg.edu.np.mad.remembertodo;
 
 import android.content.Context;
 import android.content.Intent;
+import android.view.ViewGroup;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
-import static android.R.id.text1;
-import static android.R.layout.simple_list_item_1;
+
 public class DataProvider implements RemoteViewsService.RemoteViewsFactory {
     List<String> myListView = new ArrayList<>();
     Context mContext = null;
@@ -33,8 +32,11 @@ public class DataProvider implements RemoteViewsService.RemoteViewsFactory {
         }
         @Override
         public RemoteViews getViewAt(int position) {
-            RemoteViews view = new RemoteViews(mContext.getPackageName(), R.layout.list_taskswidget);
+            RemoteViews view = new RemoteViews(mContext.getPackageName(), R.layout.category_list_widget);
+//            RemoteViews task_view = new RemoteViews(mContext.getPackageName(), R.layout.task_list_widget);
             view.setTextViewText(R.id.widgetItemTaskNameLabel, myListView.get(position));
+            
+//            view.addView(R.id.widgetItemTaskNameLabel, task_view);
             view.setInt(R.id.widgetItemTaskNameLabel,"setBackgroundResource",R.drawable.rounded_corners);
             return view;
         }
